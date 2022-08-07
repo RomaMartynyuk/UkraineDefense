bd1467cd67913ebafdafc2c0bf2e735b68548919
RomaMartynyuk <95542738+RomaMartynyuk@users.noreply.github.com>
2022-08-07 22:29:29 +0300

    WaveSpawner

---
+++ Assets/Scripts/Grade/WaveSpawner.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPreFab;
+    public Transform spawn;
+
+    public Text waveCountdown;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    private int waveNumber = 0;
+
+    private void Update()
+    {
+        if(countdown <= 0)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+        countdown -= Time.deltaTime;
+
+        waveCountdown.text = Mathf.Round(countdown).ToString();
+    }
+    IEnumerator SpawnWave()
+    {
+        Debug.Log("Наближається нова партія ворогів!");
+        waveNumber++;
+
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPreFab, spawn.position, spawn.rotation);
+    }
+}
