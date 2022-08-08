784eb15006cf42f700eaf1e6959fe5c77bdad74a
RomaMartynyuk <95542738+RomaMartynyuk@users.noreply.github.com>
2022-08-08 23:41:24 +0300

    Turret Shooting

--- Assets/Scripts/Turrets/Turret.cs
+++ Assets/Scripts/Turrets/Turret.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
     private Transform target;
-    public float range = 15f;
+
+    [Header("Set-up Unity")]
     public float turnSpeed = 10f;
     public string enemyTag = "Enemy";
+
+    public GameObject bulletPrefab;
+    public Transform firepoint;
+
+    [Header("Shooting")]
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+    public float range = 15f;
+
     private void Start()
     {
         InvokeRepeating("UpdateTarget", 0f, 0.5f);
@@ -43,6 +53,27 @@ public class Turret : MonoBehaviour
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        FireManager();
+    }
+    private void FireManager()
+    {
+        if(fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+        fireCountdown -= Time.deltaTime;
+    }
+    void Shoot()
+    {
+        GameObject bulletGO = (GameObject) Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+        if(bulletGO != null)
+        {
+            bullet.Seek(target);
+        }
     }
     private void OnDrawGizmosSelected()
     {
