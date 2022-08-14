676de18e29d1bfc09a246fdbea17328e1c04c0fe
RomaMartynyuk <95542738+RomaMartynyuk@users.noreply.github.com>
2022-08-14 14:14:47 +0300

    Missiles
    Missiles and Effects

--- Assets/Scripts/Turrets/Bullet.cs
+++ Assets/Scripts/Turrets/Bullet.cs
@@ -7,6 +7,8 @@ public class Bullet : MonoBehaviour
     private Transform target;
 
     public float speed = 50f;
+    public GameObject bulletEffect;
+    public float explosionRadius = 0f;
     public void Seek(Transform _target)
     {
         target = _target;
@@ -28,10 +30,42 @@ public class Bullet : MonoBehaviour
         }
 
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(target);
     }
     void HitTarget()
     {
-        Destroy(target.gameObject);
+        GameObject effectIns = Instantiate(bulletEffect, transform.position, transform.rotation);
+        Destroy(effectIns, 5f);
+
+        if (explosionRadius > 0f)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(target);
+        }
+        
         Destroy(gameObject);
+    }
+    void Damage(Transform enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+    void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach(Collider collider in colliders)
+        {
+            if(collider.tag == "Enemy")
+            {
+                Damage(collider.transform);
+            }
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
