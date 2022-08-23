2f86d29caf99d6b225d1191ca36c801b019d2262
RomaMartynyuk <95542738+RomaMartynyuk@users.noreply.github.com>
2022-08-23 12:46:04 +0300

    Pause Menu

--- Assets/Scripts/Grade/CameraController.cs
+++ Assets/Scripts/Grade/CameraController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private bool doMovement = true;
 
     public float panSpeed = 30f;
     public float panBorderThickness = 10f;
@@ -17,13 +16,7 @@ public class CameraController : MonoBehaviour
             this.enabled = false;
             return;
         }
-        if (Input.GetKeyDown(KeyCode.Escape)) { 
-            doMovement = !doMovement;
-        }
-        if (doMovement) 
-        { 
             CMControl();
-        }
     }
     void CMControl()
     {
